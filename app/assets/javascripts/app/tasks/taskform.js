98170bf61f5ccc4d95fe06eff944c927a072db7b
Leandro Segovia <ldlsegovia@gmail.com>
2016-10-07 11:11:58 -0300

    wip

--- app/assets/javascripts/app/tasks/taskform.js
+++ app/assets/javascripts/app/tasks/taskform.js
@@ -2,7 +2,13 @@ export default {
   props: ['categories'],
   data() {
     return  {
+      selected: null,
       options: ['hola1', 'chao1'],
     };
+  },
+  methods: {
+    updateSelected (newSelected) {
+      this.selected = newSelected;
+    }
   }
 };
